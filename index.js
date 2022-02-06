9b0dc3cc3e4804c8e3a57e61c9b8a2f032641c47
Rishabh Singh <rishabhsingh2305@gmail.com>
2022-02-06 17:47:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log('server is listining at : ', port);
+});
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
